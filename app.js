60d84651518934f7bcf21e5e3500a553d58de55b
William XING <yxing@deloitte.com.au>
2017-05-24 17:02:44 +0930

    Port updated

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.get('/', function (req, res) {
   res.send(index)
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
